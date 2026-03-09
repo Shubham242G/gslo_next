8423011d18770b3bba1312f7e8dbeddb5739afc5
Shubham242G <allhailvicky242@@gmail.com>
2026-03-09 16:43:35 +0530

    About chnage

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -35,15 +35,15 @@ export default function AboutUsPage(): JSX.Element {
     const teamMembers = [
   {
     name: 'Anusha Girish',
-    title: 'Senior Partner, Corporate Law',
+    title: 'Associate Lawyer',
     imageUrl: '/assets/anusha.jpg', 
     summary: 'Distinguished advocate with a sharp legal mind and strong commitment to justice. Graduated from Delhi University with an LLB, she fiercely advocates for clients rights, earning a reputation as a formidable legal expert',
     linkedinUrl: 'https://www.linkedin.com/in/jane-doe-67890/', 
   },
   {
     name: 'Laxman Kumar',
-    title: 'Partner, Criminal Litigation',
-    imageUrl: '/assets/laxman.jpg', 
+    title: 'Associate Lawyer',
+    imageUrl: '/assets/laxman1.jpg', 
     summary: 'With a sharp legal mind and a strong commitment to justice. He graduated from Delhi University with an LLB and is a dedicated advocate for his clients rights in the courtroom. His relentless pursuit of favorable outcomes for his clients has earned him a reputation as a formidable legal advocate.',
     linkedinUrl: 'https://www.linkedin.com/in/john-smith-11223/',
   },
@@ -95,7 +95,7 @@ export default function AboutUsPage(): JSX.Element {
       transition={{ duration: 0.8 }}
     >
       <h1 className="text-5xl md:text-7xl font-light text-white mb-6 leading-tight">
-        Your Objectives,<br />Our Purpose.
+        YouR Objectives,<br />Our Purpose.
       </h1>
 
       <p className="text-base md:text-lg text-gray-300 max-w-2xl mx-auto mb-10">
